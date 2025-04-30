205000dfbb8d280e99118ff925e81369a309c59c
Bora Erk <boraerk86@gmail.com>
2025-04-30 14:01:29 +0300

    site header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './layout.css'
+export default function Header() {
+    return (
+        <header className="site-header">
+      <img className="logo" src="src/assets/iteration-1/logo.svg"/>
+      <p>fırsatı kaçırma</p>
+      <h1 className="ana-baslık">KOD ACIKTIRIR <br/> PİZZA, DOYURUR</h1>
+      <button className="btn">ACIKTIM</button>
+      
+    </header>
+    )
+}
