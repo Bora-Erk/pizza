9ab9bdc1e571758b10f32e7e1e70fbc770a7a4e5
Bora Erk <boraerk86@gmail.com>
2025-05-02 17:08:20 +0300

    siparisalındı css

--- src/components/SiparisAlındı.jsx
+++ src/components/SiparisAlındı.jsx
@@ -21,21 +21,27 @@ export default function SiparisAlındı() {
       <Link to="/">
         <img src='src/assets/iteration-1/logo.svg' alt="Logo" />
       </Link>
-      <h1>Tebrikler, siparişiniz alındı!</h1>
+      <h5 id="lezzet">lezzetin yolda</h5>
+      <h1>SİPARİŞ ALINDI</h1>
+      <div className="pizza-title">
+        <h5>Position Absolute Acı Pizza</h5>
+      </div>
+      
 
-      <div className="order-details">
-        <p>Boyut: {data.size}</p>
-        <p>Hamur: {data.dough}</p>
-        <p>Ek malzemeler: {data.toppings.join(", ")}</p>
-        <p>Not: {data.note || "-"}</p>
-        <p>Miktar: {data.quantity}</p>
+        <div className="order-details">
+            <p>Boyut: {data.size}</p>
+            <p>Hamur: {data.dough}</p>
+            <p>Ek malzemeler: {data.toppings.join(", ")}</p>
+            <p>Not: {data.note || "-"}</p>
+            <p>Miktar: {data.quantity}</p>
+        </div>
         <div className="order-summary">
             <h6>Sipariş Toplamı</h6>
             <p>Seçimler: {data.selectionsCost}₺</p>
             <p>Toplam Fiyat: {data.totalPrice}₺</p>
         </div>
         
-      </div>
+      
     </div>
   );
 }
